e591282915174eddf321db50676fb8d645100faa
kappy <shangenjunfu9@gmail.com>
2024-01-20 11:00:52 +0900

    Update2024.1.20

---
+++ ABC/B/151~200/190.py
@@ -0,0 +1,7 @@
+n,s,d=map(int,input().split())
+xy=[list(map(int,input().split())) for _ in range(n)]
+
+for i in range(n):
+    if xy[i][0]<s and xy[i][1]>d:
+        exit(print("Yes"))
+print("No")
